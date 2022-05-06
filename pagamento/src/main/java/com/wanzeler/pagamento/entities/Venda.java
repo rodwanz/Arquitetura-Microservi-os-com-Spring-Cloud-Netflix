00dc4dd353ab2d52364ec3f6b46eb46661de22d6
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-05-06 20:26:50 -0300

    Projeto Pagamento - Classes - Produto - ProdutoVenda - Venda

---
+++ pagamento/src/main/java/com/wanzeler/pagamento/entities/Venda.java
@@ -0,0 +1,51 @@
+package com.wanzeler.pagamento.entities;
+
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "venda")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Venda implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@DateTimeFormat(pattern = "MM/dd/yyyy")
+	@Column(name = "data", nullable = false)
+	private Date data;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "venda", cascade = CascadeType.REFRESH)
+	private List<ProdutoVenda> produtos;
+	
+	@Column(name = "valorTotal", nullable = false, length = 10)
+	private Double valorTotal;
+}
---
+++ pagamento/src/test/java/com/wanzeler/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanzeler.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
